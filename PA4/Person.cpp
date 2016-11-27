978a364aeefd6e88c4e8cf948bb2a5c6cbcbe417
Dynamic-Gravity <luke4@g.clemson.edu>
2016-11-27 15:54:36 -0500

    implemented parent methods

--- PA4/Person.cpp
+++ PA4/Person.cpp
@@ -1,25 +1,19 @@
 #include "Person.h"
-void Person::setFirstName(string firstn){
-
-}
-void Person::setLastName(string lastn){
-
-}
-string Person::getFirstName(){
-
-}
-string Person::getLastName(){
-
-}
+void Person::setFirstName(string firstn){ firstName = firstn; }
+void Person::setLastName(string lastn){	lastName = lastn; }
+string Person::getFirstName(){ return firstName; }
+string Person::getLastName(){ return lastName; }
 Person::Person(){
-
+	setFirstName("");
+	setLastName("");
 }
-Person::Person(string, string){
-
+Person::Person(string first, string last){
+	setFirstName(first);
+	setLastName(last);
 }
 ~Person::Person(){
-
+	cout << "Destructor Invoked..." << endl;
 }
 void Person::printInfo(){
-	
+	cout << getFirstName() << " " << getLastName();
 }
