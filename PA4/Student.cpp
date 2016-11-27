dc6b92ad89e51106028e44cb9e820f1fa7800cb6
Dynamic-Gravity <luke4@g.clemson.edu>
2016-11-27 17:21:54 -0500

    added logic for student class

--- PA4/Student.cpp
+++ PA4/Student.cpp
@@ -1,24 +1,41 @@
 #include "Student.h"
-void Student::setGPA(float gpa){
-
-}
-void Student::setClassStanding(string standing){
-
-}
-void Student::setCurCredit(int credit){
-
-}
-float Student::getGPA(){
-
-}
-string Student::getClassStanding(){
-
-}
-int Student::getCurCredit(){
-
-}
-int Student::getMaxCredit(float g, string c){
-
+void Student::setGPA(float gpa){ GPA = gpa; }
+void Student::setClassStanding(string standing){ classStanding = standing; }
+void Student::setCurCredit(int credit){ curCredit = credit; }
+float Student::getGPA(){ return GPA; }
+string Student::getClassStanding(){ return classStanding; }
+int Student::getCurCredit(){ return curCredit; }
+int Student::getMaxCredit(float g, string s){
+	int hours
+	switch(s){
+		case "Freshman":
+		case "freshman":
+		case "FRESHMAN":
+			if (g < 2.5){ hours = 12; }
+			else { hours = 15; }
+			break;
+		case "Sophmore";
+		case "sophmore";
+		case "SOPHMORE";
+			if (g < 2.5){ hours = 12; }
+			else { hours = 18; }
+			break;
+		case "Junior";
+		case "junior";
+		case "JUNIOR";
+			if (g < 2.5){ hours = 12; }
+			else { hours = 18; }
+			break;
+		case "Senior";
+		case "senior";
+		case "SENIOR";
+			if (g < 2.5){ hours = 12; }
+			else { hours = 18; }
+			break;
+		default:
+			hours = -1;
+	}
+	return hours;
 }
 Student::Student(){
 
@@ -30,5 +47,5 @@ Student::Student(string fName, string lName, float sgpa, string sclass, int smax
 
 }
 void Student::printInfo(){
-	
+
 }
