201252e851501acc5b07ebdee1bb8fe4b1ca29f7
Dynamic-Gravity <luke4@g.clemson.edu>
2016-11-30 11:58:14 -0500

    defined methods

--- PA4/CpscCourse.cpp
+++ PA4/CpscCourse.cpp
@@ -1,34 +1,14 @@
 #include "CpscCourse.h"
-void CpscCourse::setCourseName(string){
-
-}
-void CpscCourse::setCourseNum(int){
-
-}
-void CpscCourse::setCourseSect(int){
-
-}
-void CpscCourse::setAvailSeats(int){
-
-}
-void CpscCourse::setOpenSeats(int){
-
-}
-string CpscCourse::getCourseName(){
-
-}
-int CpscCourse::getCourseNum(){
-
-}
-int CpscCourse::getCourseSect(){
-
-}
-int CpscCourse::getAvailSeat(){
-
-}
-int CpscCourse::getOpenSeats(){
-
-}
+void CpscCourse::setCourseName(string cn){ courseName = cn; }
+void CpscCourse::setCourseNum(int cn){ courseNum = cn; }
+void CpscCourse::setCourseSect(int cs){ courseSect = cs; }
+void CpscCourse::setAvailSeats(int as){ availSeats = as; }
+void CpscCourse::setOpenSeats(int os){ openSeats = os; }
+string CpscCourse::getCourseName(){ return courseName; }
+int CpscCourse::getCourseNum(){ return courseNum; }
+int CpscCourse::getCourseSect(){ return courseSect; }
+int CpscCourse::getAvailSeat(){ return availSeats; }
+int CpscCourse::getOpenSeats(){ return openSeats; }
 void CpscCourse::printInfo(){
-	
+	//needs to be done
 }
